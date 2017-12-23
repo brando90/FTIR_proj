115a45d8189ea4120de19522921d950bd6dbaf99
Brando Miranda <brando90@mit.edu>
2017-12-22 22:00:14 -0600

    mdl sgd is trianing

---
+++ mdl_trainer.py
@@ -0,0 +1,113 @@
+import time
+from datetime import date
+import calendar
+
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+#from ''' import *
+#from models_pytorch import *
+#from inits import *
+#from sympy_poly import *
+#from poly_checks_on_deep_net_coeffs import *
+#from data_file import *
+#from plotting_utils import *
+
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate
+import scipy
+from numpy.polynomial.hermite import hermvander
+
+#from maps import NamedDict
+
+import pdb
+
+import unittest
+
+def index_batch(X,batch_indices,dtype):
+    '''
+    returns the batch indexed/sliced batch
+    '''
+    if len(X.shape) == 1: # i.e. dimension (M,) just a vector
+        batch_xs = torch.FloatTensor(X[batch_indices]).type(dtype)
+    else:
+        batch_xs = torch.FloatTensor(X[batch_indices,:]).type(dtype)
+    return batch_xs
+
+def get_batch2(X,Y,M,dtype):
+    '''
+    get batch for pytorch model
+    '''
+    # TODO fix and make it nicer, there is pytorch forum question
+    X,Y = X.data.numpy(), Y.data.numpy()
+    N = len(Y)
+    valid_indices = np.array( range(N) )
+    batch_indices = np.random.choice(valid_indices,size=M,replace=False)
+    batch_xs = index_batch(X,batch_indices,dtype)
+    batch_ys = index_batch(Y,batch_indices,dtype)
+    return Variable(batch_xs, requires_grad=False), Variable(batch_ys, requires_grad=False)
+
+def get_sequential_lifted_mdl(nb_monomials,D_out, bias=False):
+    return torch.nn.Sequential(torch.nn.Linear(nb_monomials,D_out,bias=bias))
+
+def vectors_dims_dont_match(Y,Y_):
+    '''
+    Checks that vector Y and Y_ have the same dimensions. If they don't
+    then there might be an error that could be caused due to wrong broadcasting.
+    '''
+    DY = tuple( Y.size() )
+    DY_ = tuple( Y_.size() )
+    if len(DY) != len(DY_):
+        return True
+    for i in range(len(DY)):
+        if DY[i] != DY_[i]:
+            return True
+    return False
+
+def train_SGD(mdl, M,eta,nb_iter, dtype, X_train,Y_train, reg_l,R_x):
+    ##
+    #pdb.set_trace()
+    N_train,_ = tuple( X_train.size() )
+    #print(N_train)
+    for i in range(1,nb_iter+1):
+        # Forward pass: compute predicted Y using operations on Variables
+        batch_xs, batch_ys = get_batch2(X_train,Y_train,M,dtype) # [M, D], [M, 1]
+        ## FORWARD PASS
+        y_pred = mdl(batch_xs)
+        ## Check vectors have same dimension
+        if vectors_dims_dont_match(batch_ys,y_pred):
+            pdb.set_trace()
+            raise ValueError('You vectors don\'t have matching dimensions. It will lead to errors.')
+        ## LOSS + Regularization
+        if R_x is not None:
+            batch_loss = (1/M)*(y_pred - batch_ys).pow(2).sum()
+        else:
+            batch_loss = loss + reg_l*R_x
+        ## BACKARD PASS
+        batch_loss.backward() # Use autograd to compute the backward pass. Now w will have gradients
+        ## SGD update
+        for W in mdl.parameters():
+            delta = eta*W.grad.data
+            W.data.copy_(W.data - delta)
+        ## train stats
+        if i % (nb_iter/10) == 0 or i == 0:
+            #X_train_, Y_train_ = Variable(X_train), Variable(Y_train)
+            X_train_, Y_train_ = X_train, Y_train
+            current_train_loss = (1/N_train)*(mdl.forward(X_train_) - Y_train_).pow(2).sum().data.numpy()
+            print('\n-------------')
+            print('i = ',i)
+            print('current_train_loss = ',current_train_loss)
+            # print(f'eta*W.grad.data = {eta*W.grad.data}')
+            # print(f'W.grad.data = {W.grad.data}')
+        ## Manually zero the gradients after updating weights
+        mdl.zero_grad()
+    final_sgd_error = current_train_loss
+    return final_sgd_error
--- sgd_mdl.py
+++ sgd_mdl.py
@@ -9,6 +9,11 @@ from sklearn.linear_model import Ridge
 import os
 import sys
 
+import torch
+from torch.autograd import Variable
+
+from mdl_trainer import train_SGD
+
 import pdb
 
 font = {'weight' : 'normal',
@@ -18,8 +23,8 @@ matplotlib.rc('font', **font)
 
 norm_factor = 0.15
 
-""" First import A matrices & y-values
-"""
+''' First import A matrices & y-values
+'''
 path = '../BroadbandMZIdata_to_Brando'
 
 df=pd.read_csv(path+'/A1.csv', sep=',')
@@ -38,68 +43,99 @@ yval2, OPL = yf.values[:,1]/norm_factor, yf.values[:,0]
 
 wavelengths = np.linspace(1550,1570,len(A1[0]))
 
-print('wavelengths = ', wavelengths)
 '''  train '''
 A, sigma = 0.85/70., 5.0
-x_real = [A*np.exp(-(wl-1560.0)**2/sigma**2) for wl in wavelengths]
+center = 1560.0
+x_real = [A*np.exp(-(wl-center)**2/sigma**2) for wl in wavelengths]
 y_real= np.dot(A1, x_real)
 plt.plot(wavelengths, x_real)
-plt.show()
+#plt.show()
 
-""" Pseudo-inverse method (for reference) """
+''' Pseudo-inverse method (for reference) '''
 Ainv = np.linalg.pinv(A1)
 x_pinv = np.dot(Ainv, yval1)
 plt.plot(wavelengths, x_pinv)
-plt.show()
 
-ratio = np.max(yval1)/np.max(y_real)
-print("power ratio = "+str(ratio))
-""" See how close real interferogram is to measured interferogram """
-plt.plot(y_real*ratio, 'ro--', label="expected")
-plt.plot(yval1, 'ko-', label="measured")
-plt.legend(loc='best')
-plt.show()
+''' SGD '''
+##dtype = torch.cuda.FloatTensor # Uncomment this to run on GPU
+dtype = torch.FloatTensor
+##
+N,D = A1.shape
+D_out = 1
+bias=True
+''' Data set '''
+a = Variable(torch.FloatTensor(wavelengths), requires_grad=False)
+X_train, Y_train = Variable(torch.FloatTensor(A1),requires_grad=False) , Variable(torch.FloatTensor(y_real.reshape(N,1)),requires_grad=False)
+## reg params
+reg_l = 1
+A_param = Variable(torch.FloatTensor([A]), requires_grad=False)
+sigma_param = Variable(torch.FloatTensor([sigma]), requires_grad=True)
+t_param = Variable(torch.FloatTensor([center]), requires_grad=True)
+def get_reg(a,A_param,t_param,sigma_param):
+    R_x = A_param*torch.exp(-(a - t_param)**2/sigma_param**2)
+    R_x = 1/R_x
+    return R_x
+R_x = None
+R_x = get_reg(a,A_param,t_param,sigma_param)
+## SGD mdl
+M = N
+eta = 0.01
+nb_iter = 1000
+mdl_sgd = torch.nn.Sequential(torch.nn.Linear(D,D_out,bias=bias))
+#mdl_sgd.linear_layers[1].weight.data.fill_(0)
+##
+train_SGD(mdl_sgd, M,eta,nb_iter, dtype, X_train,Y_train, reg_l,R_x)
 
-""" Begin RIDGE parameter search
-"""
-alpha_list = np.logspace(-3,5,400)
-r2_list = []
-for alpha in alpha_list:
-    """ Search for a suitable alpha """
-    ridge = Ridge(alpha=alpha)
-    y_pred_ridge = ridge.fit(A1, yval1).predict(A1)
-#    r2_list.append(r2_score(yval2, np.dot(A2, ridge.coef_)))
-#    Uncomment below to optimize wrt real spectrum
-    r2_list.append(r2_score(x_real/np.max(x_real), ridge.coef_/np.max(ridge.coef_)))
 
-plt.semilogx(alpha_list, r2_list)
-plt.title("Ridge Regression Model Fitting")
-plt.xlabel("Alpha")
-plt.ylabel("R2 value")
-plt.show()
-
-max_index = r2_list.index(max(r2_list))
-alpha_max = alpha_list[max_index]
-
-ridge = Ridge(alpha=alpha_max)
-print r2_list
-print "alphamax = "+str(alpha_max)
-y_pred_ridge = ridge.fit(A2, yval2).predict(A2)
-r2_score_ridge = r2_score(x_real/np.max(x_real), ridge.coef_/np.max(ridge.coef_))
-
-print(ridge)
-print("r^2 on test data : %f" % r2_score_ridge)
-
-plt.figure(figsize=(8, 6))
-plt.subplot(2,1,1)
-plt.plot(wavelengths, ridge.coef_/np.max(ridge.coef_), 'r-', label="Ridge regression", linewidth=2.0)
-plt.legend(loc='best')
-plt.subplot(2,1,2)
-plt.plot(wavelengths, x_real/np.max(x_real), 'k-', label="Measured", linewidth=2.0)
-plt.xlabel("Wavelength [nm]", fontsize=labelsize)
-plt.ylabel("Amplitude [a.u.]", fontsize=labelsize)
-maxy, miny = 1.0, 0.0
-plt.ylim([miny - 0.05*abs(maxy-miny), maxy + 0.05*abs(maxy-miny)])
-plt.legend(loc='best')
-plt.tight_layout()
-plt.show()
+#
+# ratio = np.max(yval1)/np.max(y_real)
+# print("power ratio = "+str(ratio))
+# ''' See how close real interferogram is to measured interferogram '''
+# plt.plot(y_real*ratio, 'ro--', label="expected")
+# plt.plot(yval1, 'ko-', label="measured")
+# plt.legend(loc='best')
+# plt.show()
+#
+# ''' Begin RIDGE parameter search
+# '''
+# alpha_list = np.logspace(-3,5,400)
+# r2_list = []
+# for alpha in alpha_list:
+#     ''' Search for a suitable alpha '''
+#     ridge = Ridge(alpha=alpha)
+#     y_pred_ridge = ridge.fit(A1, yval1).predict(A1)
+# #    r2_list.append(r2_score(yval2, np.dot(A2, ridge.coef_)))
+# #    Uncomment below to optimize wrt real spectrum
+#     r2_list.append(r2_score(x_real/np.max(x_real), ridge.coef_/np.max(ridge.coef_)))
+#
+# plt.semilogx(alpha_list, r2_list)
+# plt.title("Ridge Regression Model Fitting")
+# plt.xlabel("Alpha")
+# plt.ylabel("R2 value")
+# plt.show()
+#
+# max_index = r2_list.index(max(r2_list))
+# alpha_max = alpha_list[max_index]
+#
+# ridge = Ridge(alpha=alpha_max)
+# print r2_list
+# print "alphamax = "+str(alpha_max)
+# y_pred_ridge = ridge.fit(A2, yval2).predict(A2)
+# r2_score_ridge = r2_score(x_real/np.max(x_real), ridge.coef_/np.max(ridge.coef_))
+#
+# print(ridge)
+# print("r^2 on test data : %f" % r2_score_ridge)
+#
+# plt.figure(figsize=(8, 6))
+# plt.subplot(2,1,1)
+# plt.plot(wavelengths, ridge.coef_/np.max(ridge.coef_), 'r-', label="Ridge regression", linewidth=2.0)
+# plt.legend(loc='best')
+# plt.subplot(2,1,2)
+# plt.plot(wavelengths, x_real/np.max(x_real), 'k-', label="Measured", linewidth=2.0)
+# plt.xlabel("Wavelength [nm]", fontsize=labelsize)
+# plt.ylabel("Amplitude [a.u.]", fontsize=labelsize)
+# maxy, miny = 1.0, 0.0
+# plt.ylim([miny - 0.05*abs(maxy-miny), maxy + 0.05*abs(maxy-miny)])
+# plt.legend(loc='best')
+# plt.tight_layout()
+# plt.show()
